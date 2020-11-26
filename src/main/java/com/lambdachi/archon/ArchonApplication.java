9df692f7b02912e29da84970ed9bee8b556f2278
Ethan Partin <espartin98@gmail.com>
2020-11-26 12:47:49 -0600

    Project init

---
+++ src/main/java/com/lambdachi/archon/ArchonApplication.java
@@ -0,0 +1,13 @@
+package com.lambdachi.archon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lambdachi/archon/ArchonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdachi.archon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
