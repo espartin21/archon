aacaa591049015682942efa14960d72080ed90ff
Ethan Partin <espartin98@gmail.com>
2020-11-27 22:41:25 -0600

    oops

--- src/main/java/com/github/espartin21/archon/model/groupme/attachments/GroupMeAttachment.java
+++ src/main/java/com/github/espartin21/archon/model/groupme/attachments/GroupMeAttachment.java
@@ -1,11 +1,9 @@
 package com.github.espartin21.archon.model.groupme.attachments;
 
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @NoArgsConstructor
-@AllArgsConstructor
 public abstract class GroupMeAttachment {
 }
