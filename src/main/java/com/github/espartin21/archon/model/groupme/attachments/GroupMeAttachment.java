a3d0c820837e14e3ed1b242175bbdc285fa24e32
Ethan Partin <espartin98@gmail.com>
2020-11-27 23:06:04 -0600

    same stuff earlier

--- src/main/java/com/github/espartin21/archon/model/groupme/attachments/GroupMeAttachment.java
+++ src/main/java/com/github/espartin21/archon/model/groupme/attachments/GroupMeAttachment.java
@@ -1,12 +1,14 @@
 package com.github.espartin21.archon.model.groupme.attachments;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @NoArgsConstructor
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY)
 @JsonSubTypes({
         @JsonSubTypes.Type(value = GroupMeImage.class, name = "GroupMeImage"),
         @JsonSubTypes.Type(value = GroupMeLocation.class, name = "GroupMeLocation"),
